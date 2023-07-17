5068eb30e6c1d60f430549fb5773f9b1e4006a16
UlugbekUmirov <umirovulugbek3@gmail.com>
2023-07-17 18:27:22 +0500

    first commit

--- utils/store.js
+++ utils/store.js
@@ -11,7 +11,7 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 import reducers from "./reducers";
-
+   
 let store;
 
 function initStore(initialState) {
